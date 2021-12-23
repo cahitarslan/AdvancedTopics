a737a8f0b752478bd20db778b31eeae34bae9d26
Cahit Arslan <cahitarslan.business@gmail.com>
2021-12-23 11:10:38 +0300

    Add project files.

---
+++ ActionFunc/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading;
+
+namespace ActionFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TryCatch();
+            //ActionDemo();
+
+
+            Func<int, int, int> add = Topla;
+
+            Console.WriteLine(add(4,3));
+
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            
+            Func<int> getRandomNumber1 = () => new Random().Next(1, 100);
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+
+            Console.WriteLine(getRandomNumber1());
+            Thread.Sleep(1000);
+            Console.WriteLine(getRandomNumber2());
+
+
+            //Console.WriteLine(Topla(2,3));
+
+
+
+            Console.ReadKey();
+        }
+
+        static int Topla(int x, int y)
+        {
+            return x + y;
+        }
+
+
+
+
+
+
+        private static void ActionDemo()
+        {
+            //Action
+            HandleException(() =>
+            {
+                Find();
+            });
+        }
+
+        private static void TryCatch()
+        {
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        public static void Find()
+        {
+
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Ankara", "İzmir", "Adana");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<Customer> result2 = utilities.BuildList<Customer>(new Customer { FirstName = "Cahit" }, new Customer { FirstName = "Arslan" });
+            foreach (var item in result2)
+            {
+                Console.WriteLine(item.FirstName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+
+    class Product : IEntity
+    {
+
+    }
+
+    interface IProductDal : IRepository<Product>
+    {
+    }
+
+
+    class Customer : IEntity
+    {
+        public string FirstName { get; set; }
+    }
+
+    interface ICustomerDal : IRepository<Customer>
+    {
+        void Custom();
+    }
+
+    interface IStudentDal : IRepository<Student /*int*/>
+    {
+
+    }
+
+    class Student : IEntity
+    {
+
+    }
+
+    interface IEntity
+    {
+
+    }
+
+    interface IRepository<T> where T: class, IEntity, new() /*struct*/
+    {
+        List<T> GetAll();
+        T Get(int id);
+        void Add(T product);
+        void Delete(T product);
+        void Update(T product);
+    }
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Custom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
